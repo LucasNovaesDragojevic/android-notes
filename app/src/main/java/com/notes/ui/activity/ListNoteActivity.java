c91b13d7ef613483d0fa798dfd3ad6a34fbe9a60
Lucas Novae Dragojevic <lucas.dragojevic@gmail.com>
2022-05-03 10:19:07 -0300

    Base structure

---
+++ app/src/main/java/com/notes/ui/activity/ListNoteActivity.java
@@ -0,0 +1,16 @@
+package com.notes.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.notes.R;
+
+public class ListNoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.setContentView(R.layout.activity_list_note);
+    }
+}
